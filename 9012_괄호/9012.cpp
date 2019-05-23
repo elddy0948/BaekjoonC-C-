c7a952119fca7b47a4b8f90698bc494359f8d071
김호준 <elddy0948@gmail.com>
2019-05-23 19:06:03 +0900

    9012

---
+++ 9012_괄호/9012.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int stack_size;
+int test_case;
+int flag;
+int main(){
+    cin >> test_case;
+    for(int i=0;i<test_case;i++){
+        string bracket;
+        cin>>bracket;
+        for(int i=0;i<bracket.size();i++){
+            if(bracket[i]=='('){
+                //stack in!
+                stack_size++;
+            }
+            else if(bracket[i]==')'){
+                //stack out!
+                stack_size--;
+            }
+            if(stack_size<0){
+                cout << "NO\n";
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0){
+            if(stack_size==0) cout << "YES\n";
+            else cout << "NO\n";
+        }
+        stack_size=0;
+        flag=0;
+    }
+}
